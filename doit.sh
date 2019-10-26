b668d6ff7924a22b4b87afd869f49a30bc44c110
michael werneburg <emuu@werneburg.ca>
2019-10-26 17:29:54 +0000

    removed shell script to create git repo - derp

--- doit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "# werneburg.ca" >> README.md
-git init
-git add *
-git commit -m "first commit"
-git remote add origin https://github.com/mwerneburg/werneburg.ca.git
-git push -u origin master
